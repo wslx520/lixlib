60a90d86b2099f942437e298832f1a146531f2a6
windgoner <lix9nw@gmail.com>
2012-09-13 17:52:27 +0800

    update

---
+++ Tabs.js
@@ -0,0 +1,148 @@
+﻿/* 
+类:Lix Tabs
+版本:0.4
+作者:十年灯 http://www.jo2.org/
+说明:欢迎使用,欢迎转载,但请勿据为已有
+
+*/	
+var $id=function(id){
+	return (typeof id == "object") ? id : document.getElementById(id);
+};
+var $$=function(tag,elm){
+	return elm.getElementsByTagName(tag);
+};
+var $C=function(cn,tag,elm){
+	if(!tag) tag='*';
+	var ts = $$(tag,elm);
+	var classArr = [];
+	var filter = new RegExp("(^|\\s)"+cn+"(\\s|$)");
+	for(var i=0,l=ts.length;i<l;i++){
+		if(filter.test(ts[i].className)){
+			classArr.push(ts[i]);
+		}
+	}
+	return classArr;
+}
+var cutover=function(arr,cur,cls){
+	for(var i=0,l=arr.length;i<l;i++){
+		if(i==cur){
+			var t=arr[i].className.indexOf(cls);
+			if (t==-1) {
+				arr[i].className += ' '+cls;
+			}
+			else return true;
+		}else{
+			arr[i].className = arr[i].className.replace(cls,'');
+		}
+		
+	}
+}
+var Bind = function(object, fun) {
+	return function() {
+		return fun.apply(object);
+	}
+}
+var addEvent = function(eType,eFunc,eObj){
+	eObj = eObj || document;
+	if(window.attachEvent) eObj.attachEvent("on"+eType,eFunc);
+	if(window.addEventListener) eObj.addEventListener(eType,eFunc,false);
+}
+var Tabs = function (elm,items){
+	if(elm == null){return false;}
+	return new Tabs.prototype.ini(elm,items);
+}
+Tabs.prototype = {
+	ini:function(elm,items){
+		var t=this;
+		
+		var a=items || {};
+		
+		/*开始缓存传入参数*/
+		this.hdtag =a.hdtag || 'DIV';
+		this.hdcn =a.hdcn || 'tabhd';
+		this.hdtagcur =a.hdtagcur || 'cur';
+		this.bdtag =a.bdtag || 'DIV';
+		this.bdcn =a.bdcn || 'tabbd';
+		this.bdtagcur =a.bdtagcur || 'cur';
+		this.event = a.event || 'mouseover';
+		this.delay = a.delay || 0;
+		/*缓存参数完成*/
+		
+		this.tabhd = $C(this.hdcn,this.hdtag,elm)[0];
+		this.tabtag = this.tabhd.children;
+		this.tabbd = $C(this.bdcn,this.bdtag,elm)[0];
+		this.tabcon = this.tabbd.children;
+		
+		this.now = 0;
+		this.yes = true;
+		this.time =a.auto;
+		this.sum = this.tabtag.length;
+		if(this.sum != this.tabcon.length) {
+			alert('Tab标签个数与内容个数不匹配');
+			return true;
+		}
+		//alert(t.time);
+		/**/
+		for(var i=0;i<t.sum;i++){
+			
+			(function(i){
+				addEvent('mouseover',function(){
+					t.yes = false;
+				},t.tabtag[i]);
+				addEvent(t.event,function(){
+					t.now = i;
+					//alert(i);
+					t.run = setTimeout(Bind(t,t.change),t.delay);
+				},t.tabtag[i])
+				addEvent('mouseout',function(){
+					t.yes = true;
+					clearTimeout(t.run);
+				},t.tabtag[i]);
+			})(i);
+		}
+		this.change();
+		if(this.time){
+			elm.onmouseover = function(){
+				t.stop()
+			}
+			elm.onmouseout = function(){
+				t.go()
+			}
+		}
+	},
+	change:function(){
+		cutover(this.tabtag,this.now,this.hdtagcur);
+		cutover(this.tabcon,this.now,this.bdtagcur);
+		if(this.time && this.yes){
+			this.now = (this.now == this.sum-1) ? 0 : this.now+1;
+			this.go();
+		}
+	},
+	go:function(){
+		this.stop();
+		this.auto = setTimeout(Bind(this,this.change),this.time)
+	},
+	stop:function(){
+		clearTimeout(this.auto);
+	}
+};
+Tabs.prototype.ini.prototype = Tabs.prototype;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Tabs0.5.js
@@ -0,0 +1,154 @@
+﻿/* 
+	javascript类:LixTabs
+	版本:0.5
+	作用:制作简单的多个的tabs选项卡切换效果
+	作者:十年灯 http://www.jo2.org/
+	说明:欢迎使用,欢迎转载,但请勿据为已有
+	
+*/
+var $id = function(id){
+	return (typeof id == "object") ? id : document.getElementById(id);
+},
+$$ = function(tag,elm){
+	return elm.getElementsByTagName(tag);
+},
+$C=function(cn,tag,elm){
+	if(!tag) tag='*';
+	var ts = $$(tag,elm);
+	var classArr = [];
+	var filter = new RegExp("(^|\\s)"+cn+"(\\s|$)");
+	for(var i=0,l=ts.length;i<l;i++){
+		if(filter.test(ts[i].className)){
+			classArr.push(ts[i]);
+		}
+	}
+	return classArr;
+},
+cutover=function(arr,cur,cls){
+	for(var i=0,l=arr.length;i<l;i++){
+		if(i==cur){
+			var t=arr[i].className.indexOf(cls);
+			if (t==-1) {
+				arr[i].className += ' '+cls;
+			}
+			else return true;
+		}else{
+			arr[i].className = arr[i].className.replace(cls,'');
+		}
+		
+	}
+},
+Bind = function(object, fun) {
+	return function() {
+		return fun.apply(object);
+	}
+},
+addEvent = function(eType,eFunc,eObj){
+	eObj = eObj || document;
+	if(window.attachEvent) eObj.attachEvent("on"+eType,eFunc);
+	if(window.addEventListener) eObj.addEventListener(eType,eFunc,false);
+},
+Tabs = function (elm,items){
+	if(elm == null){return false;}
+	return new Tabs.prototype.ini(elm,items);
+}
+Tabs.prototype = {
+	ini:function(elm,items){
+		var t=this;
+		
+		var a=items || {};
+		
+		/*开始缓存传入参数*/
+		t.hdtag =a.hdtag || 'DIV';
+		t.hdcn =a.hdcn || 'tabhd';
+		t.hdtagcur =a.hdtagcur || 'cur';
+		t.bdtag =a.bdtag || 'DIV';
+		t.bdcn =a.bdcn || 'tabbd';
+		t.bdtagcur =a.bdtagcur || 'cur';
+		t.event = a.event || 'mouseover';
+		t.delay = a.delay || 0;
+		/*缓存参数完成*/
+		
+		t.tabhd = $C(t.hdcn,t.hdtag,elm)[0];
+		t.tabtag = t.tabhd.children;
+		t.tabbd = $C(t.bdcn,t.bdtag,elm)[0];
+		t.tabcon = t.tabbd.children;
+		
+		t.now = 0;
+		t.yes = 1;
+		t.time =a.auto;
+		t.sum = t.tabtag.length;
+		if(t.sum != t.tabcon.length) {
+			alert('Tab标签个数与内容个数不匹配');
+			return true;
+		}
+		//alert(t.time);
+		/**/
+		var sum = t.sum;
+		for(var i=0;i<sum;i++){
+			(function(i){
+				addEvent('mouseover',function(){
+					t.yes = 0;
+				},t.tabtag[i]);
+				addEvent(t.event,function(){
+					t.now = i;
+					//alert(i);
+					t.run = setTimeout(Bind(t,change),t.delay);
+				},t.tabtag[i])
+				addEvent('mouseout',function(){
+					t.yes = 1;
+					clearTimeout(t.run);
+				},t.tabtag[i]);
+			})(i);
+		}
+		function B(fn,t) {
+			fn.call(t);
+		}
+		function change(){
+			cutover(t.tabtag,t.now,t.hdtagcur);
+			cutover(t.tabcon,t.now,t.bdtagcur);
+			if(t.time && t.yes){
+				t.now = (t.now == t.sum-1) ? 0 : t.now+1;
+				B(go,t);
+			}
+		}
+		function go(){
+			B(stop,t);
+			t.auto = setTimeout(Bind(t,change),t.time)
+		}
+		function stop(){
+			clearTimeout(t.auto);
+		}
+		if(t.time){
+			elm.onmouseover = function(){
+				B(stop,t);
+			}
+			elm.onmouseout = function(){
+				B(go,t);
+			}
+		}
+		
+		B(change,t);
+	}
+};
+Tabs.prototype.ini.prototype = Tabs.prototype;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
