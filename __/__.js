742212e4c7e3c01c038d1eab4da02cc22e3700d8
wslx520 <lix9nw@gmail.com>
2015-09-13 23:37:27 +0800

    add es5.js and __.js

---
+++ __/__.js
@@ -0,0 +1,423 @@
+//常用函数集
+var __ = function () {
+	var 
+	dom = document,
+	body = dom.body,
+	isIE = !-[1,],
+	AP = Array.prototype,
+	slice = AP.slice,
+	/*主函数*/
+	main = function  (q) {
+		return dom.querySelectorAll(q);
+	},
+	/*克隆数组或对象*/
+	clone = function  (obj,deep) {
+		var isarr = isArray(obj) ,res;
+		if(!deep) {
+			if(isarr) return slice.call(obj);
+			return extend({},obj);
+		}
+		if(isarr) {
+			res = [];
+			for(var i=0,l = obj.length;i<l;i++) {
+				var ai = obj[i];
+				res.push('object' === typeof ai ? clone(ai,true) : ai);
+			}
+			return res;		
+		} else {
+			res = {};
+			for(var i in obj) {
+				var oo = obj[i];
+				res[i] = 'object' === typeof oo ? clone(oo,true) : oo; 
+			}
+			return res;
+			
+		}
+	},
+	/* DOM */
+	contains = body.contains ? function (par,chi) {
+		return par.contains(chi);
+	} : function (par,chi) {
+		return !!(par.compareDocumentPosition(chi) & 16);
+	},
+	closest = function  (elm,fn) {
+		var par = null;
+		while(elm) {
+			if(fn(elm)) {
+				par = elm;
+				break;
+			}
+			elm = elm.parentNode;
+		}
+		return par;
+	},
+	hasClass = function (elm,cls) {
+		cls = cls.split(/\s+/);
+		var t = true;
+		for(var c=cls.length,ecls = ' '+elm.className+' ';c--;) {
+			if(ecls.indexOf(' '+cls[c]+' ') == -1) {
+				t = false; break;
+			}
+		}
+		return t;		
+	},
+	addClass = function (elm,cls) {
+		if(!hasClass(elm,cls)) {
+			elm.className = __.trim(elm.className)+' '+cls;
+		}		
+	},
+	removeClass = function (elm,cls) {
+		cls = cls.split(/\s+/);
+		cls.forEach(function  (cc,i) {
+			elm.className = elm.className.replace(RegExp('\\b'+cc+'\\b',"g"),'');
+		})				
+	},
+	addEvent = window.addEventListener? function (elm,act,fn,t) {
+		elm.addEventListener(act,fn,t || false);
+	} : function  (elm,act,fn) {
+		elm.attachEvent('on'+act,fn);
+	},
+	removeEvent = window.addEventListener ? function (elm,act,fn,t) {
+		elm.removeEventListener(act,fn,t || false);
+	} : function  (elm,act,fn) {
+		elm.detachEvent('on'+act,fn);
+	},
+	wrap = function  (nodes,par) {
+		par = par.cloneNode(true);
+		// nodes = [].slice.call(nodes);
+		if(!nodes.length) nodes=[nodes];
+		var pnode = nodes[0].parentNode;
+		var idx = index(nodes[0]);
+		for(var n =0,l=nodes.length;n<l;n++) {
+			par.appendChild(nodes[n]);
+		}	
+		pnode.insertBefore(par,pnode.children[idx]);
+	},
+	remove = function  (elem) {
+		elem.parentNode.removeChild(elem);
+	},
+	index = function(node,list) {
+		list = list || node.parentNode.children,index = -1;
+		for(var l = list.length;l--;) {
+			if(list[l] === node) {
+				// index = l;
+				return l;
+				// break;
+			}
+		}
+		return index;
+	},
+	getStyle = body.currentStyle ? function  (elm,style) {
+		return style ? elm.currentStyle[style] : elm.currentStyle;
+	} : function  (elm,style) {
+		var styles = elm.ownerDocument.defaultView.getComputedStyle(elm, null);
+		return style ? styles[style] : styles;
+	},
+	
+	/*判断*/
+	isFunction = function( f ) {
+	    // return toString.call(f) === "[object Function]";
+	    return typeof f === 'function';
+	},
+	isObject = function  (v) {
+		return toString.apply(v) === '[object Object]';
+	},
+	isArray = function(v){ 
+		return toString.apply(v) === '[object Array]'; 
+	},
+	isNumber = function  (n) {
+		return typeof n === 'number' ? true : toString.call(obj) == '[object Number]';
+	},
+	isString = function  (s) {
+		return typeof s === 'string' ? true : toString.call(obj) == '[object String]';
+	},
+	isDate = function  (d) {
+		return d instanceof Date;
+	},
+	isRegexp = function  (d) {
+		return d instanceof RegExp;
+	},
+	isNull = function  (s) {
+		return s == null;
+	},
+	isElement = function  (elm) {
+		return obj.nodeType==1 && obj.tagName;
+	},
+	typeOf = function  (obj) {
+		var t = typeof obj;
+		if(t === 'object') {
+			return (obj === null) ? 'null' :
+				isObject(obj) ? 'object' :
+				isArray(obj) ? 'array' :
+				isElement(obj) ? 'element' :
+				isRegexp(obj) ? 'regexp' :
+				isDate(obj) ? 'date' : 
+				isNumber(obj) ? 'number' : 
+				isString(obj) ? 'string' : '' ;
+		} 
+		return t;
+	},
+	/*数组*/
+	each_A = function  (arr,fn,through) {
+		for(var i=0,l=arr.length;i<l;i++) {
+			if(false === fn(arr[i],i,arr) && !through) break;
+		}
+	},
+	include_A = function  (arr,item) {
+		var f = !1;
+		each_A(arr,function  (a,i) {
+			if(a === item) {
+				f = true;
+				return false;
+			}
+		})
+		return f;
+	},
+	/*对象*/
+	each_O = function  (obj,fn,through) {
+		for(var i in obj) {
+			if(false === fn(obj[i],i,obj) && !through) break;
+		}
+	},
+	// 用新对象的属性替换老对象(会直接改变老对象)
+	extend = function  (old,newone) {
+		each_O(newone,function  (one,o) {
+			old[o] = one;
+		})
+		return old;
+	},
+	// 合并两个对象(返回一个新对象,不改变原来的两个对象)
+	merge = function  (old,newone) {
+		var r = {};
+		each_O(old,function  (one,o) {
+			r[o] = one;
+		})
+		each_O(newone,function  (one,o) {
+			r[o] = one;
+		})
+		return r;
+	},
+	/*类*/
+	Select = function  (sel,fn,through) {
+		this.select = sel;
+		var options = this.options = sel.options,
+			isFn = isFunction(fn),
+			isArr = isArray(fn),
+			aObj,
+			one = !isFn && !isArr;
+			if(isArr) {
+				one = fn.length === 1;
+				each_A(fn,function  (item,i) {
+					aObj[item] = 1;
+				})
+			}
+
+			each_A(options,function  (option,o) {
+				if(isFn) {
+					if(fn(option,o,options) === true) {
+						option.selected = true;
+						if(!through) return false;
+					}
+				} else {
+					var t = option.selected = isArr ? aObj[option.value] ==1 : fn=== option.value;
+					
+					if(t && one) {
+						return false;
+					}
+					
+				}
+			})
+			this.add =  function  (text,value) {
+				this.options.add(new Option(text,value));
+			};
+			// index可以是数字或函数
+			this.remove = function  (index,through) {
+				if(isNumber(index)) {
+					this.options.remove(i);
+				} else {
+					var options = this.options,
+						isStr = isString(index);
+					each_A(options,function  (option,o) {
+						if(isStr ? option.value == index : index(option)===true) {
+							this.options.remove(o);
+							if(!through) return false;
+						}
+					})
+				}
+				
+			} 
+	}
+	;
+	extend(main,{
+		domReady: (function () {
+	        var hasReady = false,
+	            list = [],
+	            check;
+
+	        function ready() {
+	            if (!hasReady) {
+	                hasReady = true;
+	                list.forEach(function (item) {
+	                    item();
+	                });
+	            }
+	        }
+
+	        if (document.addEventListener) {
+	            document.addEventListener('DOMContentLoaded', ready, false);
+	        } else if (window === top) {
+	            check = function () {
+	                try {
+	                    document.documentElement.doScroll('left');
+	                    ready();
+	                } catch (e) {
+	                    util.timer(check, 0);
+	                }
+	            };
+	            check();
+	        }
+
+	        addEvent(window, 'load', ready);
+
+	        return function (func) {
+	            if (hasReady) {
+	                func();
+	            } else {
+	                list.push(func);
+	            }
+	        };
+	    }()),
+	    clone:clone,
+		hasClass: hasClass,
+		removeClass: removeClass,
+		addClass: addClass,
+		toggleClass: function (elm,cls) {
+			if(hasClass(elm,cls)) {
+				removeClass(elm,cls);
+			} else {
+				addClass(elm,cls);
+			}
+		},
+		getStyle:getStyle,
+		getAttr : function (elem,attr) {
+			return elem.getAttribute(attr);
+		},
+		setAttr : function  (elem,attr,v) {
+			return elem.setAttribute(attr,v);
+		},
+		removeAttr : function  (elem,attr) {
+			return elem.removeAttribute(attr);
+		},
+		contains: contains,
+		addEvent:addEvent,
+		removeEvent:removeEvent,
+		index:index,
+		wrap:wrap,	
+		closest:closest,
+		// 方便操作select元素
+		select: function  (sel,fn,through) {
+			return new Select(sel,fn,through);
+		},	
+		each: function  (arrOrObj,fn,through) {
+			if(isObject(arrOrObj)) {
+				each_O(arrOrObj,fn,through);
+			} else {
+				each_A(arrOrObj,fn,through);
+			}
+		},
+
+		/*字符串实用函数 start*/
+		trim:function (str) {
+			return str.replace(/^\s+/, '').replace(/\s+$/, '');
+		},
+		toCamel: function  (str) {
+			return str.replace(/\-(\w)/g,function  (m,$1) {
+				return $1.toUpperCase();
+			});
+		},
+		// 包含子串
+		include : function  (str,substr) {
+			return isString(str) ? str.indexOf(substr) !== -1 : include_A(str,substr);
+		},
+		// 以指定子串开始
+		startWith : function  (str,substr,fromIndex) {
+			return str.indexOf(substr,fromIndex) === 0;
+		},
+		// 以指定子串结尾
+		endWith : function  (str,substr,fromIndex) {
+			return substr === str.slice(-substr.length);
+		},
+		/* 转换函数start */
+		toArray : function  (o) {
+			return Array.prototype.slice.call(o);
+		},
+		toInt : function  (num) {
+			return parseInt(num,10);
+		},
+		
+		/*将一个函数绑定到另一个作用域，且可指定参数*/
+		bind : function(fun,thisobj) {
+			var args = slice.call(arguments).slice(2);
+			return function() {
+				return fun.apply(thisobj, args.concat(slice.call(arguments)));
+			}
+		},
+		/*timer 支持传参*/
+		timer : function  (fn,time) {
+			var args = slice.call(arguments,2);
+			console.log(args);
+			if(args.length) {
+				return setTimeout(function  () {
+					fn.apply(fn,args);
+				},time)
+			} else {
+				return setTimeout(fn,time);
+			}	
+		},
+		extend:extend,
+		merge:merge,
+		isObject:isObject,
+		isString:isString,
+		isNumber:isNumber,
+		isNull:isNull,
+		isDate:isDate,
+		isRegexp:isRegexp,
+		isArray:isArray,
+		isFunction:isFunction,
+		typeOf:typeOf,
+		fixEvent : function  (evt) {
+			var sp = 'stopPropagation',
+			pd = 'preventDefault',
+			tt = 'target';
+			return function  (evt) {
+				evt = evt || window.event;
+				if(!evt[tt]) {
+				  	evt[tt] = evt.srcElement;
+				}
+				if(!evt[sp]) {
+				  	evt[sp] = function  () {
+				   		evt.cancelBubble = true;
+				 	}
+				}
+				if(!evt[pd]) {
+				  	evt[pd] = function  () {
+				   		evt.returnValue = false;
+				  	}
+				}
+				 // 自定义方法，会阻止默认行为，阻止冒泡，
+				if(!evt.stop) {
+				  	evt.stop = function  () {
+				   		evt[pd]();
+				   		evt[sp]();
+				  	}
+				}
+				return evt;
+			}
+		 
+		}()
+	});
+
+	return main;
+}();
+
+
---
+++ es5/es5.js
@@ -0,0 +1,204 @@
+/*
+* 使老浏览器能使用Ecma Script 5的新原生方法
+* 使用方法：在文档head中写入：
+<!--[if lte IE 8]>
+<script src="es5.js" type="text/javascript"></script>
+<![end if]-->
+*/
+!function  (undefined) {
+	
+	/*对象新方法polyfill*/
+	if(!Object.keys) {
+		Object.keys = function  (obj) {
+			var r=[];
+			for(var o in obj) {
+				r.push(o);
+			}
+			return r;
+		}
+	}
+	if(!Object.values) {
+		Object.values = function  (obj) {
+			var r=[];
+			for(var o in obj) {
+				r.push(obj[o]);
+			}
+			return r;
+		}
+	}
+	if (typeof Object.create != 'function') {
+	  Object.create = (function() {
+	    var Temp = function() {};
+	    return function (prototype) {
+	      if (typeof prototype != 'object') {
+	        throw TypeError('Argument must be an object');
+	      }
+	      Temp.prototype = prototype;
+	      var result = new Temp();
+	      Temp.prototype = null;
+	      return result;
+	    };
+	  })();
+	}
+	/*数组新方法polyfill*/
+	var ArrayPolyfill = {
+		indexOf : function(obj, idx) {
+	        var i = idx == undefined ? 0 : (idx < 0 ? Math.max(0, arr.length + idx) : idx),l = this.length;
+	        for(; i < l; i++) {
+	            if(this[i] === obj) {
+	                return i;
+	            }
+	        }
+	        return -1;
+	    },
+	    lastIndexOf : function(obj, idx) {
+	        var len = this.length, i;
+	        if(idx == undefined) {i=len}
+	        else{ 
+	          if(idx+1>len) {i = Math.min(len,idx+1)}
+	          if(idx<0){ i = Math.max(0, len + idx+1) }
+	        }
+	        for(; i--;) {
+	            if (this[i] === obj) {
+	                return i;
+	            }
+	        }
+	        return -1;
+	    },
+	    every : function(fn, thisObj) {
+	        for(var i = this.length; i--; ) {
+	            if(false === fn.call(thisObj, this[i], i, this)) {
+	                return false;
+	            }
+	        }
+	        return true;
+	    },
+	    some : function(fn, thisObj) {
+	        for(var i = this.length; i--;) {
+	            if(true === fn.call(thisObj, this[i], i, this)) {
+	                return true;
+	            }
+	        }
+	        return false;
+	    },
+	    filter : function(fn, thisObj) {
+	        for(var i = -1,l = this.length,res = []; i++<l; ) {
+                var val = this[i];
+                if(true === fn.call(thisObj, val, i, this)) {
+                    res.push(val);
+                }
+	        }
+	        return res;
+	    },
+	    map : function(fn, thisObj) {
+	        var l = this.length, res = [];
+	        for(var i = 0,l = this.length; i<l; i++){
+	            res[i] = fn.call(thisObj, this[i], i, this);
+	        }
+	        return res;
+	    },
+	    forEach : function(fn, thisObj) {
+	        for(var i = 0,l = this.length; i<l; i++) {
+	            fn.call(thisObj, this[i], i, this);
+	        }
+	    },
+	    reduce : function (callback, initialValue ) {
+		    var noinitialValue = initialValue === undefined, 
+		    	previous = noinitialValue ? this[0] : initialValue, 
+		    	k = noinitialValue ? 1 : 0, 
+		    	length = this.length;
+		    if (typeof callback === "function") {
+		      	for (k; k < length; k++) {
+		         	this.hasOwnProperty(k) && (previous = callback(previous, this[k], k, this));
+		      	}
+		    }
+		    return previous;
+		},
+		reduceRight : function (callback, initialValue ) {
+		    var noinitialValue = initialValue === undefined, length = this.length,
+		    	previous = noinitialValue ? this[length-1] : initialValue, 
+		    	k = noinitialValue ? length-2 : length-1;
+		    var length = this.length, k = length - 1, previous = initialValue;
+		    if (typeof callback === "function") {
+		        for (k; k > -1; k-=1) {          
+		          	this.hasOwnProperty(k) && (previous = callback(previous, this[k], k, this));
+		        }
+		    }
+		    return previous;
+		}
+	}
+	for(var A in ArrayPolyfill) {
+		if(!Array.prototype[A]) {
+			Array.prototype[A] = ArrayPolyfill[A];
+		}
+	}
+	// JSON
+    this.JSON = {
+        parse: function (data) {
+            return (new Function('return (' + data + ')'))();
+        },
+
+        stringify: (function () {
+            function encodeString(source) {
+                return '"' + source.replace(
+                    /["\\\x00-\x1f]/g,
+                    function (match) {
+                        return {
+                            '\b': '\\b',
+                            '\t': '\\t',
+                            '\n': '\\n',
+                            '\f': '\\f',
+                            '\r': '\\r',
+                            '"' : '\\"',
+                            '\\': '\\\\'
+                        }[match] || ('\\u00' + Math.floor(match.charCodeAt() / 16).toString(16) + (match.charCodeAt() % 16).toString(16));
+                    }
+                ) + '"';
+            }
+
+            function encodeArray(source) {
+                for (var i = 0, result = [], len = source.length; i < len; i++) {
+                    result.push(JSON.stringify(source[i]));
+                }
+                return '[' + result.join(',') + ']';
+            }
+
+            return function (value) {
+                var type = typeof value,
+                    result = [];
+
+                if (value === null || value === undefined || type === 'function') {
+                    return 'null';
+                }
+
+                if (type === 'number') {
+                    return isFinite(value) ? String(value) : 'null';
+                }
+
+                if (type === 'boolean') {
+                    return String(value);
+                }
+
+                if (type === 'string') {
+                    return encodeString(value);
+                }
+
+                if (value instanceof Array) {
+                    return encodeArray(value);
+                }
+
+                for (var key in value) {
+                    if (value.hasOwnProperty(key)) {
+                        result.push(encodeString(key) + ':' + JSON.stringify(value[key]));
+                    }
+                }
+                return '{' + result.join(',') + '}';
+            };
+        }())
+    };
+	/* Date.now */
+	Date.now = Date.now || function  () {
+		return new Date().getTime();
+	}
+
+}();
