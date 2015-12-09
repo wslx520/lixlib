9d4d0d07688dfc09d02c2d1ad123dd160695a3cd
李兴 <lixing@jingoal.com>
2015-12-09 18:15:39 +0800

    temp

--- Promise/promise.js
+++ Promise/promise.js
@@ -1,10 +1,27 @@
-var promise = function  () {
-	var list = [];
-	return {
-		resolve: function (val){},
-		reject: function (err){},
-		promise: function (){
-			
-		},
-	}
+var xPromise = function  () {
+	this.then = function (resolve, reject) {
+        var defer = new defer();
+        return defer.promise;
+    }
+    var list = [],
+        value = null,
+        state = null,
+        self = this;
+    function resolve (newValue) {
+        value = newValue;
+        state = true;
+
+    }
+    function reject (error) {
+        state = false;
+    }
+    this.promise = new promise();
+}
+function Handler(onFulfilled, onRejected, resolve, reject) {  
+    return {
+        onFulfilled: typeof onFulfilled === 'function' ? onFulfilled : null,
+        onRejected: typeof onRejected === 'function' ? onRejected : null,
+        resolve: resolve,
+        reject: reject
+    }
 }
