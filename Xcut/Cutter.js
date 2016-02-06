a20c57d3e3abe695d3dc278b9edc9c1c676a4281
李兴 <lixing@jingoal.com>
2016-02-06 12:05:24 +0800

    Cutter
    限制最小size时的调整，未完成

--- Xcut/Cutter.js
+++ Xcut/Cutter.js
@@ -1,5 +1,6 @@
 (function (dom) {
     'use strict';
+    var min = Math.min, max = Math.max;
     function create(cls, tag) {
         tag = dom.createElement(tag || 'div');
         tag.className = cls;
@@ -9,6 +10,12 @@
         var temp = dom.createElement('div');
         temp.innerHTML = str;
         return temp.firstChild;
+    }
+    function extend (old, newone) {
+        for(var n in newone) {
+            old[n] = newone[n];
+        }
+        return old;
     }
     // 获得按下的这个节点的坐标
     // 本来可以直接用getMouse当前按下的点，但这是不精确的
@@ -52,43 +59,52 @@
         // 采用起止点办法
         // 分为上下左右四个函数(左上则是由上和左拼起来的，依此类推)
         // 变量缩写：m:mouse,r:rect,v:visualRect
-        function R(m, r, v) {
+        var v = {x:0,y:0,X:0,Y:0};
+        function R(m, r) {
             v.x = min(m.x, r.x);
             v.X = max(m.x, r.x);
+            return v;
         }
-        function L(m, r, v) {
+        function L(m, r) {
             v.x = min(m.x, r.X);
             v.X = max(m.x, r.X);
+            return v;
         }
-        function T(m, r, v) {
+        function T(m, r) {
             v.y = min(m.y, r.Y);
             v.Y = max(m.y, r.Y);
+            return v;
         }
 
-        function B(m, r, v) {
+        function B(m, r) {
             v.y = min(m.y, r.y);
             v.Y = max(m.y, r.y);
+            return v;
         }
         return {
             l:L,t:T,r:R,b:B,
-            tl: function (m, r, v) {
-                T(m, r, v);
-                L(m, r, v);
+            tl: function (m, r) {
+                T(m, r);
+                L(m, r);
+                return v;
             },
-            tr: function (m, r, v) {
-                T(m, r, v);
-                R(m, r, v);
+            tr: function (m, r) {
+                T(m, r);
+                R(m, r);
+                return v;
             },
-            bl: function (m, r, v) {
-                B(m, r, v);
-                L(m, r, v);
+            bl: function (m, r) {
+                B(m, r);
+                L(m, r);
+                return v;
             },
-            br: function (m, r, v) {
-                B(m, r, v);
-                R(m, r, v);
+            br: function (m, r) {
+                B(m, r);
+                R(m, r);
+                return v;
             }
         }
-    }(Math.min, Math.max));
+    }(min, max));
     /*
     * options选项：
     * * size:最小尺寸，默认值无。可以是一个数组，两个值依次表示width,height;也可以是一个数字，则矩形会变成正方形
@@ -132,8 +148,8 @@
         function getMouse(e) {
             mouse.x = e.clientX - cutbox.L;
             mouse.y = e.clientY - cutbox.T + dom.body.scrollTop + dom.documentElement.scrollTop;
-            mouse.x = Math.min(Math.max(0, mouse.x), cutbox.width);
-            mouse.y = Math.min(Math.max(0, mouse.y), cutbox.height);
+            mouse.x = min(max(0, mouse.x), cutbox.width);
+            mouse.y = min(max(0, mouse.y), cutbox.height);
             return mouse;
         }
         function checkRect () {
@@ -153,17 +169,12 @@
         this.copy = copy = elem.cloneNode();
         // copy.style.left = 
         this.status = 'draw';
-        divs = rectBox.children;
-        for (i = 0; i < 4; i += 1) {
-            boxes.push(divs[i]);
-        }
-        this.resizer = boxes;
         cutbox.appendChild(mask);
         cutbox.appendChild(va);
         cutbox.appendChild(copy);
         cutbox.appendChild(rectBox);
         elem.parentNode.appendChild(cutbox);
-        rectBox.style.cssText = 'width:'+size+'px;height:'+size+'px;';
+        rectBox.style.cssText = 'width:'+root._w+'px;height:'+root._h+'px;';
         rectBox.style.left = (cutbox.offsetWidth - size)/2 + 'px';
         rectBox.style.top = (cutbox.offsetHeight - size)/2 + 'px';
 
@@ -171,9 +182,6 @@
         cutbox.T = cutbox.getBoundingClientRect().top;
         cutbox.height = cutbox.offsetHeight;
         cutbox.width = cutbox.offsetWidth;
-        function fixVisualRect () {
-            
-        }
         function documentOnMouseMove (e) {
             e = e || window.event;
             e.preventDefault();
@@ -185,7 +193,7 @@
                 visualRect.x += mouse.x - start.x;
                 visualRect.y += mouse.y - start.y; 
                 // 修正范围
-                root.move(Math.min(Math.max(visualRect.x,0), cutbox.width - visualRect.w), Math.min(Math.max(visualRect.y,0), cutbox.height - visualRect.h));   
+                root.move(min(max(visualRect.x,0), cutbox.width - visualRect.w), min(max(visualRect.y,0), cutbox.height - visualRect.h));   
                 // 必须是赋值而不是覆盖
                 start.x = mouse.x;
                 start.y = mouse.y;
@@ -194,13 +202,28 @@
                     // return;
                 }
                 // visualRect = calculate(start, mouse, rect, visualRect);
-                getCoords(mouse, rect, visualRect);
-                _w = visualRect.X - visualRect.x;
-                _h = visualRect.Y - visualRect.y;
-                if(root.square) {
-                    _w = _h = Math.max(root._w, Math.min(_w, _h));
-                }
+                var coords = getCoords(mouse, rect, visualRect);
+                _w = max(coords.X - coords.x, root._w);
+                _h = max(coords.Y - coords.y, root._h);
                 if(_w <= root._w ||  _h <= root._h) return;
+                if(root.square) {
+                    var t = min(_w, _h);
+                    console.log(coords, visualRect);
+                    if(coords.x !== visualRect.x) {
+                        visualRect.x = coords.x + t - _w;
+                    }
+                    if(coords.X !== visualRect.X) {
+                        visualRect.X = coords.X + t - _w;
+                    }
+                    if(coords.y !== visualRect.y) {
+                        visualRect.y = coords.y + t - _w;
+                    }
+                    if(coords.Y !== visualRect.Y) {
+                        visualRect.Y = coords.Y + t - _w;
+                    }
+                    
+                    // visualRect.Y += t - _h;
+                }
                 // visualRect.w = _w;
                 // visualRect.h = _h;
                 root.draw(visualRect);
@@ -220,7 +243,7 @@
             // 必须是赋值而不是覆盖
             start.x = mouse.x;
             start.y = mouse.y;
-            console.log(start);
+            // console.log(start);
             visualRect = getBound(rectBox);
             dom.addEventListener('mousemove', documentOnMouseMove);
         };
